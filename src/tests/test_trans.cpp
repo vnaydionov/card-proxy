d416ad78963983ce771d052360aa881843f5a25b
Viacheslav Naydenov <vnaydionov@gmail.com>
2016-01-14 16:15:07 +0300

    fixing configs and scripts, step2

--- src/tests/test_trans.cpp
+++ src/tests/test_trans.cpp
@@ -8,7 +8,7 @@ int main()
     Yb::LogAppender appender(std::cerr);
     Yb::Logger::Ptr logger(new Yb::Logger(&appender));
     logger->debug("started");
-    Yb::Session session(Yb::theSchema(), "mysql+soci://user=card_proxy_usr pass=card_proxy_pwd host=127.0.0.1 port=23306 service=card_proxy_db");
+    Yb::Session session(Yb::theSchema(), "mysql+soci://user=card_proxy_user pass=card_proxy_pass host=127.0.0.1 port=3306 service=card_proxy_db");
     session.set_logger(logger->new_logger("yb"));
     session.debug("hello from session");
     session.engine()->logger()->debug("hello from engine");
